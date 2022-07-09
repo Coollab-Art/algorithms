bc41a7880832cd83f5fca1ea88be5855c3f1771d
TaliaKah <anaissaratagougne@tutanota.com>
2022-07-09 17:09:58 +0200

    ✨ [algorithms] Add translocate_element()

--- include/algorithms/algorithms.hpp
+++ include/algorithms/algorithms.hpp
@@ -1,7 +1,26 @@
 #pragma once
-
+// inspired from : https://stackoverflow.com/a/57399634
 namespace algorithms {
 
+template<typename Container>
+void translocate_element(Container&, typename Container::iterator current_location, typename Container::iterator new_location)
+{
+    if (current_location > new_location)
+    {
+        std::rotate(
+            new_location,
+            current_location,
+            current_location + 1
+        );
+    }
+    else
+    {
+        std::rotate(
+            std::make_reverse_iterator(new_location) - 1,
+            std::make_reverse_iterator(current_location) - 1,
+            std::make_reverse_iterator(current_location)
+        );
+    }
+}
 
-    
 } // namespace algorithms
