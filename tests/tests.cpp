c6cb1869cccadd5adb0bf4e5852ecd08c12759e5
TaliaKah <anaissaratagougne@tutanota.com>
2022-07-10 15:56:12 +0200

    ✅ Works with vector and array

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,134 +1,139 @@
 #define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
 #include <doctest/doctest.h>
 #include <algorithms/algorithms.hpp>
+#include <array>
+#include <vector>
 
 // Check out doctest's documentation: https://github.com/doctest/doctest/blob/master/doc/markdown/tutorial.md
 
 // Allow doctest to print std::vectors
-namespace std {
-template<typename T>
-std::ostream& operator<<(std::ostream& output, std::vector<T> const& values)
+
+template<typename Container>
+doctest::String toString(const Container& values)
 {
-    for (auto const& value : values)
+    std::stringstream output{};
+    for (const auto& value : values)
     {
         output << value << ", ";
     }
-    return output;
+    return doctest::String{output.str().c_str()};
 }
-} // namespace std
+
+TYPE_TO_STRING(std::vector<int>);
+TYPE_TO_STRING(std::array<int, 5>);
 
 // TODO templatized test on vecor, list etc.
-TEST_CASE("translocate_element() moves one element from one place to another in a container")
+TEST_CASE_TEMPLATE("translocate_element() moves one element from one place to another in a container", Container, std::vector<int>, std::array<int, 5>)
 {
-    auto vec = std::vector<int>{0, 1, 2, 3, 4};
+    auto container = Container{0, 1, 2, 3, 4};
 
     SUBCASE("When moving backward")
     {
         algorithms::translocate(
-            vec.begin() + 3,
-            vec.begin() + 1
+            std::next(container.begin(), 3),
+            std::next(container.begin(), 1)
         );
 
-        CHECK(vec == std::vector{0, 3, 1, 2, 4});
+        CHECK(container == Container{0, 3, 1, 2, 4});
     }
 
     SUBCASE("When moving forward")
     {
         algorithms::translocate(
-            vec.begin() + 2,
-            vec.begin() + 3
+            std::next(container.begin(), 2),
+            std::next(container.begin(), 3)
         );
 
-        CHECK(vec == std::vector{0, 1, 3, 2, 4});
+        CHECK(container == Container{0, 1, 3, 2, 4});
     }
 
     SUBCASE("When moving to the beginning")
     {
         algorithms::translocate(
-            vec.begin() + 4,
-            vec.begin()
+            std::next(container.begin(), 4),
+            container.begin()
         );
 
-        CHECK(vec == std::vector{4, 0, 1, 2, 3});
+        CHECK(container == Container{4, 0, 1, 2, 3});
     }
 
     SUBCASE("When moving the first element")
     {
         algorithms::translocate(
-            vec.begin(),
-            vec.begin() + 3
+            container.begin(),
+            std::next(container.begin(), 3)
         );
 
-        CHECK(vec == std::vector{1, 2, 3, 0, 4});
+        CHECK(container == Container{1, 2, 3, 0, 4});
     }
 
     SUBCASE("When moving to the end")
     {
         algorithms::translocate(
-            vec.begin() + 3,
-            vec.end() - 1
+            std::next(container.begin(), 3),
+            std::prev(container.end(), 1)
         );
 
-        CHECK(vec == std::vector{0, 1, 2, 4, 3});
+        CHECK(container == Container{0, 1, 2, 4, 3});
     }
 
     SUBCASE("When moving the last element")
     {
         algorithms::translocate(
-            vec.end() - 1,
-            vec.begin() + 2
+            std::prev(container.end(), 1),
+            std::next(container.begin(), 2)
         );
 
-        CHECK(vec == std::vector{0, 1, 4, 2, 3});
+        CHECK(container == Container{0, 1, 4, 2, 3});
     }
 
     SUBCASE("When staying at the same location")
     {
         algorithms::translocate(
-            vec.begin() + 2,
-            vec.begin() + 2
+            std::next(container.begin(), 2),
+            std::next(container.begin(), 2)
         );
 
-        CHECK(vec == std::vector{0, 1, 2, 3, 4});
+        CHECK(container == Container{0, 1, 2, 3, 4});
     }
 
     SUBCASE("When moving the first element to the same location")
     {
         algorithms::translocate(
-            vec.begin(),
-            vec.begin()
+            container.begin(),
+            container.begin()
         );
 
-        CHECK(vec == std::vector{0, 1, 2, 3, 4});
+        CHECK(container == Container{0, 1, 2, 3, 4});
     }
 
     SUBCASE("When moving the last element to the same location")
     {
         algorithms::translocate(
-            vec.end() - 1,
-            vec.end() - 1
+            std::prev(container.end(), 1),
+            std::prev(container.end(), 1)
         );
 
-        CHECK(vec == std::vector{0, 1, 2, 3, 4});
+        CHECK(container == Container{0, 1, 2, 3, 4});
     }
 
     SUBCASE("When moving the last element to the beginning")
     {
         algorithms::translocate(
-            vec.end() - 1,
-            vec.begin()
+            std::prev(container.end(), 1),
+            container.begin()
         );
 
-        CHECK(vec == std::vector{4, 0, 1, 2, 3});
+        CHECK(container == Container{4, 0, 1, 2, 3});
     }
 
     SUBCASE("When moving the first element to the end")
     {
         algorithms::translocate(
-            vec.begin(),
-            vec.end() - 1
+            container.begin(),
+            std::prev(container.end(), 1)
         );
 
-        CHECK(vec == std::vector{1, 2, 3, 4, 0});
+        CHECK(container == Container{1, 2, 3, 4, 0});
     }
 }
